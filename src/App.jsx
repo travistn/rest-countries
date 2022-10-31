c7ead70243f74e044bec032d710bc5e995704a2e
travistn <travisn714@gmail.com>
2022-10-31 15:02:27 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <>App</>;
+  return <></>;
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-dark-blue': 'hsl(209, 23%, 22%)',
+        'color-very-dark-blue-dm': 'hsl(207, 26%, 17%)',
+        'color-very-dark-blue-lm': 'hsl(200, 15%, 8%)',
+        'color-dark-gray': 'hsl(0, 0%, 52%)',
+        'color-very-light-gray': 'hsl(0, 0%, 98%)',
+        'color-white': 'hsl(0, 0%, 100%)',
+      },
+    },
   },
   plugins: [],
 };
