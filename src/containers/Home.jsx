e00f53656b5184f57271f75d2d35eba0df4450c4
travistn <travisn714@gmail.com>
2022-10-31 18:08:46 -0700

    Added a dropdown menu for filtering regions

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div className='w-screen min-h-screen'>
       <Header />
       <div className='flex flex-col items-center'>
-        <form className='flex flex-row items-center justify-center pl-8 w-[343px] h-[48px] mt-8 rounded-[5px] gap-4 shadow-search'>
+        <form className='flex flex-row items-center pl-8 w-[343px] h-[48px] mt-8 rounded-[5px] gap-4 shadow-search'>
           <label>
             <AiOutlineSearch className='text-[#B2B2B2]' />
           </label>
@@ -21,6 +21,14 @@ const Home = () => {
             placeholder='Search for a country...'
           />
         </form>
+        <select className='w-[200px] h-[48px] mt-8 shadow-search text-[12px] leading-[20px] font-normal p-4 rounded-[5px] mr-auto ml-4'>
+          <option>Filter by Region</option>
+          <option>Africa</option>
+          <option>America</option>
+          <option>Asia</option>
+          <option>Europe</option>
+          <option>Oceania</option>
+        </select>
       </div>
     </div>
   );
