0f302238d07ea0234c5890da2b30d1f4530588b3
travistn <travisn714@gmail.com>
2022-11-01 13:57:21 -0700

    Added a button that displays more countries on click

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 import Header from '../components/Header';
+import CountryCard from '../components/CountryCard';
 
 const Home = () => {
   const [search, setSearch] = useState('');
+  const [countryCount, setCountryCount] = useState(10);
+  const [countries, setCountries] = useState();
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then((res) => setCountries(res.data));
+  }, []);
 
   return (
     <div className='w-screen min-h-screen'>
@@ -29,6 +37,16 @@ const Home = () => {
           <option>Europe</option>
           <option>Oceania</option>
         </select>
+        <div className='flex flex-col gap-8 mt-8'>
+          {countries?.slice(0, countryCount)?.map((country) => (
+            <CountryCard country={country} />
+          ))}
+        </div>
+        <button
+          className='w-[264px] h-[50px] mt-8 mb-8 bg-[#1eb3df] text-white text-[18px] font-semibold rounded-md'
+          onClick={() => setCountryCount((prevCount) => prevCount + 10)}>
+          Load More
+        </button>
       </div>
     </div>
   );
