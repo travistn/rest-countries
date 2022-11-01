f39cc138d9c8353839aa126721a1d132b7154cd3
travistn <travisn714@gmail.com>
2022-11-01 16:22:55 -0700

    Added box shadow for back button

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         header: '0px 2px 4px rgba(0, 0, 0, 0.0562443)',
         search: '0px 2px 9px rgba(0, 0, 0, 0.0532439);',
         countryCard: '0px 0px 7px 2px rgba(0, 0, 0, 0.0294384)',
+        backButton: '0px 0px 7px rgba(0, 0, 0, 0.293139)',
       },
     },
   },
