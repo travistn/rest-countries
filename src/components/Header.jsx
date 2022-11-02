5bfffaf6069b5754165825dde8381e4437d91467
travistn <travisn714@gmail.com>
2022-11-02 16:28:32 -0700

    Added dark mode functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FiMoon } from 'react-icons/fi';
 
+import useDarkMode from '../hooks/useDarkMode';
+
 const Header = () => {
-  const [darkModeToggle, setDarkModeToggle] = useState(false);
+  const [colorTheme, setColorTheme] = useDarkMode();
   const navigate = useNavigate();
 
-  console.log(darkModeToggle);
-
   return (
-    <div className={`${darkModeToggle && 'dark'}`}>
-      <div className='flex flex-row w-[screen] h-[80px] items-center p-4 lg:pl-12 lg:pr-12 shadow-header dark:bg-color-dark-blue'>
-        <h5
-          className='text-[14px] lg:text-[24px] leading-[20px] lg:leading-[33px] font-extrabold lg:hover:cursor-pointer dark:text-white'
-          onClick={() => navigate('/')}>
-          Where in the world?
-        </h5>
-        <div
-          className='flex flex-row items-center ml-auto gap-2 lg:hover:cursor-pointer'
-          onClick={() => setDarkModeToggle(!darkModeToggle)}>
-          <FiMoon className='text-[14px] lg:text-[16px] dark:fill-white' />
-          <p className='font-semibold text-[12px] lg:text-[16px] leading-[16px] lg:leading-[22px] dark:text-white'>
-            Dark Mode
-          </p>
-        </div>
+    <div className='flex flex-row w-[screen] h-[80px] items-center p-4 lg:pl-12 lg:pr-12 shadow-header dark:bg-color-dark-blue'>
+      <h5
+        className='text-[14px] lg:text-[24px] leading-[20px] lg:leading-[33px] font-extrabold lg:hover:cursor-pointer dark:text-white'
+        onClick={() => navigate('/')}>
+        Where in the world?
+      </h5>
+      <div
+        className='flex flex-row items-center ml-auto gap-2 lg:hover:cursor-pointer'
+        onClick={() => setColorTheme(colorTheme)}>
+        <FiMoon className='text-[14px] lg:text-[16px] dark:fill-white' />
+        <p className='font-semibold text-[12px] lg:text-[16px] leading-[16px] lg:leading-[22px] dark:text-white'>
+          Dark Mode
+        </p>
       </div>
     </div>
   );
