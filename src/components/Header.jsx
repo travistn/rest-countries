98667104be19ea96594d1409e4fbb9cf4b38dd6a
travistn <travisn714@gmail.com>
2022-11-01 22:41:47 -0700

    Clicking on the header will redirect to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FiMoon } from 'react-icons/fi';
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='flex flex-row w-[screen] h-[80px] items-center p-4 lg:pl-12 lg:pr-12 shadow-header'>
-      <h5 className='text-[14px] lg:text-[24px] leading-[20px] lg:leading-[33px] font-extrabold '>
+      <h5
+        className='text-[14px] lg:text-[24px] leading-[20px] lg:leading-[33px] font-extrabold lg:hover:cursor-pointer'
+        onClick={() => navigate('/')}>
         Where in the world?
       </h5>
       <div className='flex flex-row items-center ml-auto gap-2'>
