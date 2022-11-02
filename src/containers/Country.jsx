870134a8ac33c0aad248e93322ca879bc0ea7967
travistn <travisn714@gmail.com>
2022-11-02 15:16:14 -0700

    Added a desktop view for the country page

--- src/containers/Country.jsx
+++ src/containers/Country.jsx
@@ -26,68 +26,82 @@ const Country = () => {
   return (
     <div className='w-screen min-h-screen'>
       <Header />
-      <div className='flex flex-col p-8 gap-8'>
+      <div className='flex flex-col p-8 lg:p-16 gap-8'>
         <button
-          className='w-[104px] h-[32px] flex flex-row items-center justify-center gap-2 text-[14px] leading-[20px] rounded-[2px] shadow-backButton'
+          className='w-[104px] h-[32px] lg:w-[136px] lg:h-[40px] flex flex-row items-center justify-center gap-2 text-[14px] lg:text-[16px] leading-[20px] rounded-[2px] shadow-backButton'
           onClick={() => navigate(-1)}>
-          <BiArrowBack className='text-[16px]' />
+          <BiArrowBack className='text-[16px] lg:text-[18px]' />
           Back
         </button>
-        <div className='flex flex-col mt-4 gap-8'>
-          <img src={country?.flags.png} alt='flag' className='w-[320px] h-[229px] rounded-md' />
-          <div className='flex flex-col gap-4'>
-            <h5 className='text-[22px] leading-[30px] font-extrabold'>{country?.name.common}</h5>
-            <div className='flex flex-col text-[14px] leading-[32px] font-extrabold'>
-              <p>
-                Official Name: <span className='font-normal'>{country?.name.official}</span>
-              </p>
-              <p>
-                Population:{' '}
-                <span className='font-normal'>{country?.population.toLocaleString()}</span>
-              </p>
-              <p>
-                Region: <span className='font-normal'>{country?.region}</span>
-              </p>
-              <p>
-                Sub Region: <span className='font-normal'>{country?.subregion}</span>
-              </p>
-              <p>
-                Capital: <span className='font-normal'>{country?.capital}</span>
-              </p>
+        <div className='flex flex-col lg:flex-row mt-4 gap-8 lg:items-center'>
+          <img
+            src={country?.flags.png}
+            alt='flag'
+            className='w-[320px] h-[229px] lg:w-[560px] lg:h-[401px] rounded-md'
+          />
+          <div className='flex flex-col gap-4 lg:ml-12'>
+            <h5 className='text-[22px] lg:text-[32px] leading-[30px] lg:leading-[44px] font-extrabold'>
+              {country?.name.common}
+            </h5>
+            <div className='flex flex-col lg:flex-row gap-4 lg:gap-[6rem] lg:mt-4'>
+              <div className='flex flex-col text-[14px] lg:text-[16px] leading-[32px] font-extrabold'>
+                <p>
+                  Official Name: <span className='font-normal'>{country?.name.official}</span>
+                </p>
+                <p>
+                  Population:{' '}
+                  <span className='font-normal'>{country?.population.toLocaleString()}</span>
+                </p>
+                <p>
+                  Region: <span className='font-normal'>{country?.region}</span>
+                </p>
+                <p>
+                  Sub Region: <span className='font-normal'>{country?.subregion}</span>
+                </p>
+                <p>
+                  Capital: <span className='font-normal'>{country?.capital}</span>
+                </p>
+              </div>
+              <div className='flex flex-col text-[14px] lg:text-[16px] leading-[32px] font-extrabold'>
+                <p>
+                  Top Level Domain: <span className='font-normal'>{country?.tld}</span>
+                </p>
+                <p>
+                  Currencies:{' '}
+                  {country?.currencies &&
+                    Object.values(country?.currencies)?.map((currency, index) => (
+                      <span className='font-normal' key={currency?.name}>
+                        {(index ? ', ' : '') + currency?.name}
+                      </span>
+                    ))}
+                </p>
+                <p>
+                  Languages:{' '}
+                  {country?.languages &&
+                    Object.values(country?.languages).map((language, index) => (
+                      <span className='font-normal' key={language}>
+                        {(index ? ', ' : '') + language}
+                      </span>
+                    ))}
+                </p>
+              </div>
             </div>
-            <div className='flex flex-col text-[14px] leading-[32px] font-extrabold'>
-              <p>
-                Top Level Domain: <span className='font-normal'>{country?.tld}</span>
-              </p>
-              <p>
-                Currencies:{' '}
-                {country?.currencies &&
-                  Object.values(country?.currencies)?.map((currency, index) => (
-                    <span className='font-normal' key={currency?.name}>
-                      {(index ? ', ' : '') + currency?.name}
-                    </span>
-                  ))}
-              </p>
-              <p>
-                Languages:{' '}
-                {country?.languages &&
-                  Object.values(country?.languages).map((language, index) => (
-                    <span className='font-normal' key={language}>
-                      {(index ? ', ' : '') + language}
-                    </span>
-                  ))}
-              </p>
-            </div>
-            <div className='flex flex-col gap-4'>
-              <h5 className='text-[16px] leading-[24px] font-semibold'>Border Countries:</h5>
+            <div className='flex flex-col gap-4 lg:mt-6'>
+              <h5 className='text-[16px] lg:text-[16px] leading-[24px] font-semibold'>
+                Border Countries:
+              </h5>
               <div className='flex flex-row flex-wrap gap-2'>
-                {borderCountries?.map((border) => (
-                  <button
-                    className='w-[96px] h-[28px] text-[12px] leading-[16px] shadow-borderCountry rounded-[2px]'
-                    onClick={() => navigate(`/country/${border?.name.common}`)}>
-                    {border?.name.common}
-                  </button>
-                ))}
+                {borderCountries ? (
+                  borderCountries?.map((border) => (
+                    <button
+                      className='w-[100px] lg:w-[120px] h-[32px] lg:h-[40px] text-[12px] lg:text-[14px] leading-[16px] lg:leading-[19px] shadow-borderCountry rounded-[2px] lg:hover:cursor-pointer lg:hover:bg-gray-100'
+                      onClick={() => navigate(`/country/${border?.name.common}`)}>
+                      {border?.name.common}
+                    </button>
+                  ))
+                ) : (
+                  <p>There are no border countries.</p>
+                )}
               </div>
             </div>
           </div>
